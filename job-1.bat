7059d45597f5481a81475525c0382524c229153a
rtayek <rtayek@gmail.com>
2021-05-15 14:53:05 -0700

    trying to get a shell script to work so make job-1.bat simple.

--- job-1.bat
+++ job-1.bat
@@ -1,12 +1,2 @@
-rem needs to be a clone, but then second build fails, so wipe workspace first
-rem git clone https://github.com/rtayek/chief.git
-dir chief
-
 rem https://tudip.com/blog-post/configure-jenkins-job-to-run-batch-command/
-rem foo.bat does not work unless custom workspace.
-rem but chief\foo.bat does work fine!
-rem git init --quiet
-rem git pull --quiet https://github.com/rtayek/chief.git
-rem gradle --quiet
-rem do something not so dangerous since we are now running in the current directory.
 echo hello
