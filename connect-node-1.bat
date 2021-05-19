2fcc7d80c03c0c440528e35cdc5a3a317846cb1c
rtayek <rtayek@gmail.com>
2021-05-18 19:44:48 -0700

    freestyle with gradle and junit reports seem to work

--- connect-node-1.bat
+++ connect-node-1.bat
@@ -3,11 +3,6 @@ REM connect this agent (slave) node to controller (master) node.
 rem but first create the agent on the controller note.
 rem TCP port for inbound agents must be enabled
 REM http://192.168.1.6:8080/jnlpJars/agent.jar
-REM Run from agent command line:
-set secret=432b8d912e934cda93a606afeebae0fd
-rem run in parent folder
-java -jar ../jars/agent.jar -jnlpUrl http://127.0.0.1:8080/computer/agent-99/jenkins-agent.jnlp -secret %secret% -workDir "d:\jenkins-agent-99"
- 
-REM Run from agent command line, with the secret stored in a file:
-REM echo e28da1a9e4f143228f5ba32b1c307b34 > secret-file
-REM java -jar agent.jar -jnlpUrl http://localhost:8080/computer/agent-1/jenkins-agent.jnlp -secret @secret-file -workDir "c:\jenkins-node-1"
+java -jar ../jars/agent.jar -jnlpUrl http://localhost:8080/computer/agent-99/jenkins-agent.jnlp -secret @my-secret.txt -workDir "d:\jenkins-agent-99"
+rem need to enable tls for agent to connect
+rem trying D:\jenkins-agent-99 for remote root directory.
--- start-jenkins.bat
+++ start-jenkins.bat
@@ -1,16 +1,6 @@
-rem run this is d:\jenkins
 rem make all these things environment variables!
 cd
 set JENKINS_HOME=d:\jenkins\jenkins-home
-rem set JENKINS_HOME="D:\jenkins\jenkins-home"
-
-"C:\Program Files\Java\jdk-11.0.1\bin\java.exe" -jar ..\jars\jenkins.war --enable-future-java --httpPort=8080
-REM 432b8d912e934cda93a606afeebae0fd
-
-rem https://superuser.com/questions/100249/how-to-pin-either-a-shortcut-or-a-batch-file-to-the-new-windows-7-8-and-10-task
-rem https://stackoverflow.com/questions/38318719/run-a-shortcut-with-a-batch-file/38318765
-rem start /c CommandPrompt1.lnk run-batch-files.bat
-rem start /d .. "run-jenkins.bat" CommandPrompt2.lnk
-rem start "connect-node-1.bat" CommandPrompt3.lnk
-rem start "try something" CommandPrompt4.lnk
-
+rem let's try java 11
+rem "C:\Program Files\Java\jdk-11.0.1\bin\java.exe" -jar ..\jars\jenkins.war --enable-future-java --httpPort=8080
+"C:\Program Files\Java\jre1.8.0_281\bin\java.exe" -jar ..\jars\jenkins.war --enable-future-java --httpPort=8080
